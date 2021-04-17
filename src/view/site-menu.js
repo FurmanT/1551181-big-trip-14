3ca2b421cfa9f0d86b7bcad3c1da7ce344302e61
FurmanT <silence19@mail.ru>
2021-04-17 16:13:32 +0500

    Время разбивать камни

---
+++ src/view/site-menu.js
@@ -0,0 +1,6 @@
+export const createSiteMenuTemplate = () => {
+  return `<nav className="trip-controls__trip-tabs  trip-tabs">
+  <a className="trip-tabs__btn" href="#">Table</a>
+  <a className="trip-tabs__btn  trip-tabs__btn--active" href="#">Stats</a>
+</nav>`;
+}
