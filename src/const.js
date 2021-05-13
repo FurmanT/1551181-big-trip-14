7a57032f5fed69b70aa66a22af2875beccf72072
FurmanT <silence19@mail.ru>
2021-05-13 08:29:25 +0500

    6.5. Обновление века (часть 1)

--- src/const.js
+++ src/const.js
@@ -1,37 +1,158 @@
-export const CITIES = ['Barcelona', 'Santorini','Heraklion', 'Rim'];
-export const OPTIONS = [
-  {
-    id: 1,
-    title: 'Switch to comfort',
-    price: 80,
-    name: 'comfort',
+export const DESTINATIONS = [
+  { description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Chamonix',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Chamonix parliament building',
+      }],
   },
-  {
-    id: 2,
-    title: 'Add luggage',
-    price: 30,
-    name: 'luggage',
+  { description: 'Barcelona, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Barcelona',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.111111111111111',
+        description: 'Barcelona parliament building',
+      }],
   },
-  {
-    id: 3,
-    title: 'Add meal',
-    price: 15,
-    name: 'meal',
+  { description: 'Santorini, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Santorini',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.222222222222222',
+        description: 'Santorini parliament building',
+      }],
   },
-  {
-    id: 4,
-    title: 'Choose seats',
-    price: 5,
-    name: 'seats',
+  { description: 'Heraklion, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Heraklion',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.231334444444444',
+        description: 'Heraklion parliament building',
+      }],
   },
-  {
-    id: 5,
-    title: 'Travel by train',
-    price: 40,
-    name: 'train',
+  { description: 'Rim, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Rim',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.2313342344444444',
+        description: 'Rim parliament building',
+      }],
   },
 ];
-export const TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Transport', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+
+export const OPTIONS =[
+  {
+    'type': 'taxi',
+    'offers': [
+      {
+        'title': 'Upgrade to a business class',
+        'price': 120,
+      },
+      {
+        'title': 'Choose the radio station',
+        'price': 60,
+      },
+      {
+        'title': 'Add luggage',
+        'price': 30,
+      },
+    ],
+  },
+  {
+    'type': 'flight',
+    'offers': [
+      {
+        'title': 'Upgrade to a business class',
+        'price': 120,
+      },
+      {
+        'title': 'Switch to comfort',
+        'price': 120,
+      },
+      {
+        'title': 'Add meal',
+        'price': 15,
+      },
+    ],
+  },
+  {
+    'type': 'ship',
+    'offers': [
+      {
+        'title': 'Upgrade to a business class',
+        'price': 80,
+      },
+      {
+        'title': 'Add luggage',
+        'price': 30,
+      },
+    ],
+  },
+  {
+    'type': 'bus',
+    'offers': [
+      {
+        'title': 'Choose seats',
+        'price': 5 ,
+      },
+      {
+        'title': 'Add luggage',
+        'price': 30 ,
+      },
+    ],
+  },
+  {
+    'type': 'train',
+    'offers': [
+      {
+        'title': 'Choose seats',
+        'price': 5,
+      },
+      {
+        'title': 'Add meals',
+        'price': 15,
+      },
+      {
+        'title': 'Add luggage',
+        'price': 30,
+      },
+    ],
+  },
+  {
+    'type': 'restaurant',
+    'offers': [
+      {
+        'title': 'Choose seats',
+        'price': 5,
+      },
+    ],
+  },
+  {
+    'type': 'sightseeing',
+    'offers': [
+      {
+        'title': 'Travel by train',
+        'price': 40,
+      },
+    ],
+  },
+  {
+    'type': 'drive',
+    'offers': [
+      {
+        'title': 'Choose seats',
+        'price': 5,
+      },
+      {
+        'title': 'Add luggage',
+        'price': 30,
+      },
+    ],
+  },
+];
+
+export const TYPES = ['taxi', 'bus', 'train', 'ship', 'transport', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 export const  DESCRIPTION = [
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   'Cras aliquet varius magna, non porta ligula feugiat eget.',
--- src/view/point-edit.js
+++ src/view/point-edit.js
@@ -1,6 +1,7 @@
-import {CITIES, TYPES, OPTIONS} from '../const';
+import {DESTINATIONS, TYPES } from '../const';
 import AbstractView from './abstract.js';
 import dayjs from 'dayjs';
+import {getOptionsByType} from '../mock/options';
 
 const BLANK_POINT = {
   type: '',
@@ -15,11 +16,11 @@ const BLANK_POINT = {
   price: 0,
 };
 
-const createPointEditOptionsTemplate = (currentOptions) => {
-  return OPTIONS.map((option)=>
+const createPointEditOptionsTemplate = (currentOptions, optionsByType) => {
+  return optionsByType.map((option)=>
     `<div class="event__offer-selector">
      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${option.name}-1" type="checkbox"
-           name="event-offer-${option.name}" ${(currentOptions.indexOf(option.id) === -1 ? '' : 'checked')}>
+           name="event-offer-${option.name}" ${(currentOptions.indexOf(option) === -1 ? '' : 'checked')}>
       <label class="event__offer-label" htmlFor="event-offer-${option.name}-1">
         <span class="event__offer-title">${option.title}</span>
         &plus;&euro;&nbsp;
@@ -33,7 +34,7 @@ const createEventTypeTemplate = (current) => {
     `<div class="event__type-item">
       <input id="event-type-${type.toLowerCase()}-1" class="event__type-input  visually-hidden" type="radio" name="event-type"
            value="${type.toLowerCase()}" ${current === type ? 'checked' : ''}>
-      <label class="event__type-label  event__type-label--${type.toLowerCase()}" htmlFor="event-type-${type}-1">${type}</label>
+      <label data-type='${type}'  class="event__type-label  event__type-label--${type.toLowerCase()}" htmlFor="event-type-${type}-1">${type}</label>
     </div>`).join('');
 };
 
@@ -41,21 +42,23 @@ const createCityTemplate = (current) => {
   return `<input class="event__input  event__input--destination" id="event-destination-1" type="text"
            name="event-destination" value="${current}" list="destination-list-1">
           <datalist id="destination-list-1">
-            ${CITIES.map((city) => {
-    return `<option value="${city}"></option>`;
+            ${DESTINATIONS.map((destination) => {
+    return `<option value="${destination.name}"></option>`;
   }).join('')}
           </datalist>`;
 };
 
 const createPointEditTemplate = (point) => {
-  const {type, destination,startDate, endDate, price , options } = point;
+  const {type, destination, startDate, endDate, price, options} = point;
+  const optionType = getOptionsByType(type);
+
   return `<li class="trip-events__item">
   <form class="event event--edit" action="#" method="post">
     <header class="event__header">
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+          <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
         <div class="event__type-list">
@@ -101,7 +104,8 @@ const createPointEditTemplate = (point) => {
       <section class="event__section  event__section--offers">
         <h3 class="event__section-title  event__section-title--offers">Offers</h3>
         <div class="event__available-offers">
-            ${createPointEditOptionsTemplate(options)}
+          ${createPointEditOptionsTemplate(options, optionType)}
+        </div>
       </section>
       <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
@@ -111,9 +115,9 @@ const createPointEditTemplate = (point) => {
 
     <div class="event__photos-container">
       <div class="event__photos-tape">
-      ${destination.photos ?
-    (destination.photos.map((photo) =>
-      `<img class="event__photo" src="${photo}" alt="Event photo">`)) : ''}
+      ${destination.pictures ?
+    (destination.pictures.map((photo) =>
+      `<img class="event__photo" src="${photo.src}" alt="${photo.description}">`)) : ''}
       </div>
     </div>
    </form>
@@ -123,18 +127,93 @@ const createPointEditTemplate = (point) => {
 export default class PointEdit extends AbstractView {
   constructor(point = BLANK_POINT) {
     super();
-    this._point = point;
+    this._state = PointEdit.parsePointToState(point);
     this._formSubmitHandler = this._formSubmitHandler.bind(this);
     this._closeClickHandler = this._closeClickHandler.bind(this);
+    this._destinationClickHandler = this._destinationClickHandler.bind(this);
+    this._typeClickHandler = this._typeClickHandler.bind(this);
+    this._setInnerHandlers();
+  }
+
+  static parsePointToState(point) {
+    return Object.assign(
+      {},
+      point,
+      {
+        isOption: point.options.length !== 0,
+      },
+    );
+  }
+
+  static parseStateToPoint(state) {
+    state = Object.assign({}, state);
+
+    return state;
+  }
+
+  restoreHandlers() {
+    this._setInnerHandlers();
+    this.setFormSubmitHandler(this._callback.formSubmit);
+  }
+
+  _setInnerHandlers() {
+    this.getElement()
+      .querySelector('.event__type-group')
+      .addEventListener('click', this._typeClickHandler);
+    this.getElement()
+      .querySelector('.event__input--destination')
+      .addEventListener('change', this._destinationClickHandler);
+  }
+
+  _destinationClickHandler(evt) {
+    evt.preventDefault();
+    const selectedDestination = DESTINATIONS.find((destination) => destination.name === evt.target.value);
+    this.updateData({
+      destination: selectedDestination,
+    });
+  }
+
+  _typeClickHandler(evt) {
+    if (evt.target.tagName !== 'LABEL') {
+      return;
+    }
+    evt.preventDefault();
+    this.updateData({
+      type: evt.target.dataset.type,
+      options: [],
+    });
+  }
+
+  updateElement() {
+    const prevElement = this.getElement();
+    const parent = prevElement.parentElement;
+    this.removeElement();
+    const newElement = this.getElement();
+    parent.replaceChild(newElement, prevElement);
+    this.restoreHandlers();
+  }
+
+  updateData(update) {
+    if (!update) {
+      return;
+    }
+
+    this._state = Object.assign(
+      {},
+      this._state,
+      update,
+    );
+
+    this.updateElement();
   }
 
   getTemplate() {
-    return createPointEditTemplate(this._point);
+    return createPointEditTemplate(this._state);
   }
 
   _formSubmitHandler(evt) {
     evt.preventDefault();
-    this._callback.formSubmit();
+    this._callback.formSubmit(PointEdit.parseStateToPoint(this._state));
   }
 
   setFormSubmitHandler(callback) {
